7ca47509d377b8c58443c4aa6e592aa967a31a48
kaju <alperensaygin@gmail.com>
2019-08-26 00:21:47 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,8 @@
+Config                            = {}
+Config.Locale                     = 'tr'
+
+Config.HackSure    = 10 --Kaç dakikada bir kredi kartı hack yapabileceğini ayarlar
+Config.PolisiAra           = true -- Hack sonrası polis alarmını aktif etmek için true ayarlayın
+Config.PolisiAramaYuzdesi    = 2 -- 1=100%, 2=50%, 3=33%, 4=25%, 5=20%.
+Config.BeklemeSuresi = 3 --Polisler blipi kaç saniye görsün?
+Config.HackerTablet = true -- TRUE yaparsanız hack için hacker tableti gerekir
---
+++ hackerjob.sql
@@ -0,0 +1,11 @@
+INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+(9547, 'hackertablet', 'Hacker Tableti', -1, 0, 1),
+
+
+INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
+('hacker', 'Hacker', 1);
+
+
+
+INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+(987, 'hacker', 0, 'hacker', 'hacker', 0, '{}', '{}');
